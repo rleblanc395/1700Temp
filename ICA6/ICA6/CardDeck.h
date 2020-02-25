5f4933aac43e3f341d77b1e29e562da5bc3072a2
rleblanc395 <59615255+rleblanc395@users.noreply.github.com>
2020-02-25 09:49:38 -0700

    l

--- ICA6/ICA6/CardDeck.h
+++ ICA6/ICA6/CardDeck.h
@@ -1,1 +1,38 @@
 #pragma once
+
+
+typedef unsigned int uint;
+typedef enum suit
+{
+	Clubs = 1,
+	Diamonds,
+	Hearts,
+	Spades
+}Suits;
+
+typedef enum value
+{
+	Ace = 1,
+	Deuce,
+	Three,
+	Four,
+	Five,
+	Six,
+	Seven,
+	Eight,
+	Nine,
+	Ten,
+	Jack,
+	Queen,
+	King
+}CardValue;
+
+typedef struct card
+{
+	Suits suit;
+	uint face;
+	CardValue value;
+}Card;
+
+Card deck[52];
+
---
+++ ICA6/ICA6/Main.c
@@ -0,0 +1,8 @@
+#include "CardDeck.h"
+
+
+
+int main(int argc, char* argv)
+{
+
+}
